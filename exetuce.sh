1a3f812fa1f23739a50a6bc2bd0c703f96dd05c9
gorecki gracjan <gorecki.gracjan@hotmail.com>
2020-11-25 15:22:57 +0100

    changes

--- exetuce.sh
+++ exetuce.sh
@@ -1,9 +1,13 @@
 sudo apt install tilix tlp chrome-gnome-shell lm-sensors git gnome-tweak-tool zsh neovim goldendict -y
 sudo apt update && sudo apt upgrade -y
 
-cp -rf * ~/
+mkdir ~/workspace
+
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+cp -rf * ~/
 
 wget https://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_20.04/amd64/albert_0.16.1_amd64.deb
 
@@ -19,7 +23,5 @@ gsettings set org.gnome.desktop.interface clock-format '24h'
 
 #curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 #~/.cargo/bin/cargo install procs bat ripgrep hunter exa
-
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 bash -c  "$(wget -qO- https://git.io/vQgMr)"
